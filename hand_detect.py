8ccec9cea127a3038977d61db34f397e88177ca7
thesourabhk <skumardas267@gmail.com>
2022-06-24 20:20:11 +0530

    hand detection model added

--- hand_detect.py
+++ hand_detect.py
@@ -1,14 +1,24 @@
 import cv2
+import mediapipe as mp
 
 cap = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier('./haarcascade_palm.xml')
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
 
-while cap.isOpened():
-    _, img = cap.read()
-    face = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=4)
-    for(x, y, w, h) in face:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    if cv2.waitKey(1) == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+while True:
+    success, image = cap.read()
+    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(imageRGB)
+    # checking whether a hand is detected
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:  # working with each hand
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = image.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                if id == 20:
+                    cv2.circle(image, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+
+            mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
+            cv2.imshow("Output", image)
+            cv2.waitKey(1)
